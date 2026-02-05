582028dd9de2b0a149eb47044eaa0725b9e119a1
AsInike <asinike.ratana@student.cadt.edu.kh>
2026-02-05 11:23:11 +0700

    BLA-001

---
+++ lib/services/rides_service.dart
@@ -0,0 +1,40 @@
+import 'package:quiz/model/ride_pref/ride_pref.dart';
+
+import '../data/dummy_data.dart';
+import '../model/ride/locations.dart';
+import '../model/ride/ride.dart';
+
+////
+///   This service handles:
+///   - The list of available rides
+///
+class RidesService {
+  static List<Ride> allRides = fakeRides;
+
+  static List<Ride> filterByDeparture(Location departure) {
+    return allRides
+        .where((ride) => ride.departureLocation == departure)
+        .toList();
+  }
+
+  static List<Ride> filterBySeatRequested(int seatRequested) {
+    return allRides
+        .where((ride) => ride.availableSeats >= seatRequested)
+        .toList();
+  }
+
+  static List<Ride> filterBy({
+    Location? departure,
+    int? seatRequested,
+  }) {
+    return allRides.where((ride) {
+      final matchDeparture =
+          departure == null || ride.departureLocation == departure;
+
+      final matchSeats =
+          seatRequested == null || ride.availableSeats >= seatRequested;
+
+      return matchDeparture && matchSeats;
+    }).toList();
+  }
+}
